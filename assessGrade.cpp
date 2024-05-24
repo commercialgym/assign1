5a0bad5b91a55d002d7d3000e6c7eb7c2f970e85
commercialgym <159087491+commercialgym@users.noreply.github.com>
2024-05-24 19:06:41 -0400

    assessGrade(double) and assessGrade(int [])

--- assessGrade.cpp
+++ assessGrade.cpp
@@ -8,7 +8,11 @@
 #pragma warning(disable: 4996)
 
 #define SUCCESS 0
-#define ERROR 0
+#define ERROR -1
+#define PASS 1
+#define FAIL 2
+#define SPECIAL_CASE 3
+
 
 
 //letter grade to be converted
@@ -19,20 +23,56 @@ int assessGrade(char*)
 	return 0;
 }
 
-//final grade worker bee
-int assessGrade(double)
+
+//FUNCTION: assessGrade(double)
+//DESCRIPTION: final grade worker bee, assesses whether the value passed was a passing
+// grade, failing grade, or an invalid value
+//PARAMETERS: double
+//RETURNS: either a pass, fail, or error (meaning invalid grade)
+int assessGrade(double finalGrade)
 {
+	if (finalGrade >= 0.00 && finalGrade <= 100.00)
+	{
+		if (finalGrade >= 54.50)
+		{
+			return PASS;
+		}
+		if (finalGrade < 54.50)
+		{
+			return FAIL;
+		}
+	}
 
-	return 0;
-
+	//else means that the input was invalid
+	return ERROR;
 }
 
 //list of 5 integer grades
-int assessGrade(int[])
+int assessGrade(int listOfGrades[])
 {
+	int gradeSum = 0;
+	double finalGrade = 0;
+	int numGrades = 5;
+
+	for (int i = 0; i < numGrades; i++)
+	{
+		if (listOfGrades[i] >= 0 && listOfGrades[i] <= 100)
+		{
+			gradeSum += listOfGrades[i];
+		}
+		else
+		{
+			//if there is ever an input among the list that is not within range, immediately
+			//quit this function and tell the parseUserInput function that there was an invalid input
+			return ERROR;
+		}
+	}
+
+	finalGrade = (double)gradeSum / (double)numGrades;
+	//now send to worker bee to assess whether it is a pass or fail
+	assessGrade(finalGrade);
 
 	return 0;
-
 }
 
 //intelligently parses userInput
